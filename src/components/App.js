dc1381a8b2f286e01bbfed7e8cf6100919bcd039
Pauline Muigai <wanjirumuigai@gmail.com>
2023-09-12 16:57:46 +0300

    added the view one movie component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import Featured from "./Featured";
 import Home from "./Home";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import OneMovieContainer from "./OneMovieContainer";
 
 function App() {
   return (
     <div>
-      <Sidebar />
+      <OneMovieContainer />
       {/* <Home /> */}
     </div>
   );
--- src/components/OneMovieCard.jsx
+++ src/components/OneMovieCard.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const OneMovieCard = () => {
-  return <div></div>;
+  const [movie, setMovies] = useState([]);
+
+  const url = "http://localhost:3000";
+
+  useEffect(() => {
+    fetch("http://localhost:3000/films/2")
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
+  return (
+    <div className="one-movie-card">
+      <div className="item-1">
+        <img src={movie.Image} />
+      </div>
+      <div className="item-2"></div>
+      <div className="item-3"></div>
+      <div className="item-4"></div>
+      <div className="item-5"></div>
+      <div className="item-6"></div>
+    </div>
+  );
 };
 
 export default OneMovieCard;
