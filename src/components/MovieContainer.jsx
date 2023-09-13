219a014a29f8e7a23df3e828d11f9aea136642c5
wanjirumuigai <wanjirumuigai@gmail.com>
2023-09-13 21:09:19 +0300

    working on making the homepage responsive

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 
 function MovieContainer({ movies, setMovies }) {
   return (
-    <ul className="grid md:grid-cols-2 lg:grid-cols-4 mx-5">
+    <ul className="grid md:grid-cols-2 lg:grid-cols-4 ml-12 md:mx-auto gap-3 md:w-9/12">
       <MovieCard movies={movies} setMovies={setMovies} />
     </ul>
   );
