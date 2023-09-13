ea870dbfe77238436396fa3edb14ba3b237293fb
Pauline Muigai <wanjirumuigai@gmail.com>
2023-09-13 17:13:44 +0300

    made the view one movie page responsive

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,54 +5,60 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { SiImdb } from "react-icons/si";
+import { GiTomato } from "react-icons/gi";
 
 const MovieCard = ({ movies, setMovies }) => {
   const navigate = useNavigate();
   const [genres, setGenre] = useState([]);
 
   function handleView(id) {
-    console.log(id);
     navigate(`/movie/${id}`);
   }
   return (
     <>
       {movies.map((movie) => {
         return (
-          <div
-            className="movie"
-            data-testid="movie-card"
-            onClick={() => handleView(movie.id)}
-            key={movie.id}
-          >
-            <FavoriteBorderIcon className="love-heart" />
+          <Link>
+            <div
+              className="movie"
+              data-testid="movie-card"
+              onClick={() => handleView(movie.id)}
+              key={movie.id}
+            >
+              <FavoriteBorderIcon className="love-heart" />
 
-            <img
-              data-testid="movie-poster"
-              src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
-              width={300}
-              height={450}
-            />
+              <img
+                data-testid="movie-poster"
+                src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
+                width={300}
+                height={450}
+              />
+              {console.log(movies)}
+              <div className="movie__infos">
+                <h2 className="font-bold" data-testid="movie-title">
+                  {movie.title}
+                </h2>
+              </div>
 
-            <div className="movie__infos">
+              <div className="flex flex-row justify-between gap-1">
+                <div className="flex flex-row gap-2">
+                  <SiImdb className=" bg-yellow-500 text-xl" />{" "}
+                  <p className="text-xs">{movie.vote_average}</p>
+                </div>
+                <div className="flex flex-row gap-2">
+                  {/* <img
+                    className="text-sm"
+                    src="https://img.icons8.com/?size=2x&id=erXgYlZE3JhX&format=png"
+                  /> */}
+                  <GiTomato className=" text-xl" />
+                  <p className="text-xs">${Math.round(movie.popularity)}%</p>
+                </div>
+              </div>
               <p data-testid="movie-release-date">{movie.release_date}</p>
-              <h2 className="font-bold" data-testid="movie-title">
-                {movie.original_title}
-              </h2>
             </div>
-
-            <div className="movie__imdb">
-              <a
-                href={movie.imdbLink}
-                className="movie__imdb-button"
-                target="blank"
-              >
-                {" "}
-                IMDb{" "}
-              </a>
-              <p>{movie.Genre}</p>
-            </div>
-          </div>
+          </Link>
         );
       })}
     </>
