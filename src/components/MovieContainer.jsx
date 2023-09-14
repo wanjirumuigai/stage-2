a648168526ff310fc5737c3e771b84b4a905523f
Pauline Muigai <wanjirumuigai@gmail.com>
2023-09-14 15:15:09 +0300

    added favorite movies title

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -2,12 +2,23 @@ import React from "react";
 
 import "./App.css";
 import MovieCard from "./MovieCard";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { Link } from "@mui/material";
 
 function MovieContainer({ movies, setMovies }) {
   return (
-    <ul className="grid md:grid-cols-2 lg:grid-cols-4 ml-12 md:mx-auto gap-3 md:w-9/12">
-      <MovieCard movies={movies} setMovies={setMovies} />
-    </ul>
+    <>
+      <section className="flex flex-row justify-between md:mx-auto md:w-9/12 my-5">
+        <h3>Featured Movies</h3>
+        <Link className="flex flex-row items-center no-underline text-red-600">
+          <span className="no-underline text-red-600">see more</span>
+          <MdOutlineArrowForwardIos />
+        </Link>
+      </section>
+      <ul className="grid md:grid-cols-2 lg:grid-cols-4 ml-12 md:mx-auto gap-3 md:w-9/12">
+        <MovieCard movies={movies} setMovies={setMovies} />
+      </ul>
+    </>
   );
 }
 export default MovieContainer;
